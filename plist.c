beb0ce2793253bd196368c4829c03dc077b2d8ac
Lord <103863076+Lord83700@users.noreply.github.com>
2025-01-08 16:59:51 +0100

    Update affichage + Ajoute du temps

--- plist.c
+++ plist.c
@@ -82,6 +82,22 @@ void PrintThreadInfo(DWORD processID) {
     CloseHandle (hsnapshot);
 }
 
+void formatTime(FILETIME ft, char *buffer, size_t bufferSize) {
+    
+    ULARGE_INTEGER uli;
+    uli.LowPart = ft.dwLowDateTime;
+    uli.HighPart = ft.dwHighDateTime;
+
+   
+    unsigned long long totalMilliseconds = uli.QuadPart / 10000;
+    unsigned int hours = totalMilliseconds / (1000 * 60 * 60);
+    unsigned int minutes = (totalMilliseconds / (1000 * 60)) % 60;
+    unsigned int seconds = (totalMilliseconds / 1000) % 60;
+    unsigned int milliseconds = totalMilliseconds % 1000;
+
+    snprintf(buffer, bufferSize, "%02u:%02u:%02u.%03u", hours, minutes, seconds, milliseconds);
+}
+
 
 void Plist()
 {
@@ -89,15 +105,70 @@ void Plist()
     PROCESSENTRY32 pe32;
     pe32.dwSize = sizeof(PROCESSENTRY32);
 
+    const char *headers[] = {"Name", "PID", "Pri", "Thd", "Priv", "CPU TIME", "Elapsed Time"};
+
+    printf("%-50s %-10s %-10s %-10s %-10s %-10s %-10s\n", headers[0], headers[1], headers[2], headers[3], headers[4], headers[5], headers[6]);
+    printf("---------------------------------------------------------------------------------------------------------------------------\n");
+
     if (Process32First(hSnapshot, &pe32)) {
         do {
-            DWORD processId = pe32.th32ProcessID;
-            DWORD Pri = pe32.pcPriClassBase;
-            DWORD Thd = pe32.cntThreads;
-            SIZE_T MemVirt = GetMemoryVirt(processId);
-            _tprintf(TEXT("Processus : %s\n    PID : %d\n    MemVirt : %zu octets\n    Priority : %d\n    Nombre de Thread: %d\n"), pe32.szExeFile, processId, MemVirt, Pri, Thd);
-            PrintThreadInfo(processId);
+            HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
 
+            if (hProcess){
+                DWORD processId = pe32.th32ProcessID;
+                DWORD Pri = pe32.pcPriClassBase;
+                DWORD Thd = pe32.cntThreads;
+                SIZE_T MemVirt = GetMemoryVirt(processId);
+
+                FILETIME creationTime, exitTime, kernelTime, userTime;
+                char cpuTimeBuffer[20] = "N/A";
+                char elapsedTimeBuffer[20] = "N/A";
+
+                if (GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime)) {
+                    
+                    ULARGE_INTEGER user, kernel;
+                    user.LowPart = userTime.dwLowDateTime;
+                    user.HighPart = userTime.dwHighDateTime;
+                    kernel.LowPart = kernelTime.dwLowDateTime;
+                    kernel.HighPart = kernelTime.dwHighDateTime;
+
+                    FILETIME totalCpuTime;
+                    totalCpuTime.dwLowDateTime = user.LowPart + kernel.LowPart;
+                    totalCpuTime.dwHighDateTime = user.HighPart + kernel.HighPart;
+                    formatTime(totalCpuTime, cpuTimeBuffer, sizeof(cpuTimeBuffer));
+
+                    
+                    SYSTEMTIME nowSystemTime;
+                    FILETIME nowFileTime;
+                    GetSystemTime(&nowSystemTime);
+                    SystemTimeToFileTime(&nowSystemTime, &nowFileTime);
+
+                    ULARGE_INTEGER now, creation;
+                    now.LowPart = nowFileTime.dwLowDateTime;
+                    now.HighPart = nowFileTime.dwHighDateTime;
+                    creation.LowPart = creationTime.dwLowDateTime;
+                    creation.HighPart = creationTime.dwHighDateTime;
+
+                    if (now.QuadPart > creation.QuadPart) {
+                        ULARGE_INTEGER elapsed;
+                        elapsed.QuadPart = now.QuadPart - creation.QuadPart;
+                        FILETIME elapsedTime;
+                        elapsedTime.dwLowDateTime = elapsed.LowPart;
+                        elapsedTime.dwHighDateTime = elapsed.HighPart;
+                        formatTime(elapsedTime, elapsedTimeBuffer, sizeof(elapsedTimeBuffer));
+                    }
+                }
+
+                printf("%-50s %-10d %-10d %-10d %-10lu %-10s  %-10s\n",
+                    pe32.szExeFile,
+                    processId,
+                    Pri,
+                    Thd,
+                    MemVirt,  //  dwSize placeholder pour la private memory
+                    cpuTimeBuffer,
+                    elapsedTimeBuffer);
+                //PrintThreadInfo(processId);
+            }
         } while (Process32Next(hSnapshot, &pe32));
     }
     CloseHandle(hSnapshot);
@@ -105,6 +176,7 @@ void Plist()
 
 int main( void )
 {
+    
     Plist();
 
     return 0;
