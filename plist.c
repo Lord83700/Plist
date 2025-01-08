cc889b7bb56feaf21dde8dd002e9efc3ffde6fc9
Nintae01 <168721053+Nintae01@users.noreply.github.com>
2025-01-08 11:24:12 +0100

    Update plist.c (add threadinfo function)
    j'ai pas le state pour l'instant

--- plist.c
+++ plist.c
@@ -20,6 +20,55 @@ SIZE_T GetMemoryVirt(DWORD processID){
     }
 }
 
+void PrintThreadInfo(DWORD processID) {
+    HANDLE hsnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD,0);
+    if(hsnapshot == INVALID_HANDLE_VALUE){
+        _tprintf(TEXT("Unable to create thread snapshot.\n"));
+        return;
+    }
+
+    THREADENTRY32 te32;
+    te32.dwSize = sizeof(THREADENTRY32);
+
+    if(Thread32First(hsnapshot, &te32)){
+        do{
+            if(te32.th32OwnerProcessID==processID){
+                DWORD threadID = te32.th32ThreadID;
+
+                HANDLE hThread = OpenThread(THREAD_QUERY_INFORMATION,FALSE,threadID);
+
+                DWORD lastError = 0;
+                CONTEXT context = {0};
+                context.ContextFlags = CONTEXT_FULL;
+
+                if(hThread !=NULL ){
+
+                    LPVOID entryPoint = NULL;
+                    lastError = GetLastError();
+
+                    _tprintf(TEXT("   Thread ID:  %lu\n"), threadID);
+                    _tprintf(TEXT("   Entry Point: .%lu\n"), entryPoint);
+                    _tprintf(TEXT("   State%lu\n"));
+                    _tprintf(TEXT("   Last Error: %lu\n"), lastError);
+
+
+                    CloseHandle(hThread);
+                }else{
+                    _tprintf(TEXT("   Thread ne s'ouvre pas %lu.\n"), threadID);
+                }
+            }
+        }while(Thread32Next(hsnapshot, &te32));
+
+    }
+
+    CloseHandle (hsnapshot);
+
+
+
+
+}
+
+
 void PrintProcessNameAndID()
 {
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -33,6 +82,7 @@ void PrintProcessNameAndID()
             DWORD Thd = pe32.cntThreads;
             SIZE_T MemVirt = GetMemoryVirt(processId);
             _tprintf(TEXT("Processus : %s\n    PID : %d\n    MemVirt : %zu octets\n    Priority : %d\n    Thread: %d\n"), pe32.szExeFile, processId, MemVirt, Pri, Thd);
+            PrintThreadInfo(processId);
 
         } while (Process32Next(hSnapshot, &pe32));
     }
